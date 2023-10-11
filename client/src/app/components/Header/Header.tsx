5276bd67598507b9666809b00da55e8cc00523a5
r-bytes <101632683+r-bytes@users.noreply.github.com>
2023-10-11 23:30:25 +0200

    general styling

--- client/src/app/components/Header/Header.tsx
+++ client/src/app/components/Header/Header.tsx
@@ -1,14 +1,16 @@
 import { ConnectWallet } from "@thirdweb-dev/react"
 import { ThemeToggle } from ".."
+import { useTheme } from "next-themes"
 
 type Props = {}
 const Header = (props: Props) => {
+    const { theme, setTheme } = useTheme()
     return (
         <>
-            <h1 className="text-3xl"> AATBA </h1>
-
+            <h1 className="text-3xl px-4"> AATBA </h1>
             <div className="flex items-center justify-between space-x-4">
                 <ConnectWallet
+                    theme={theme === "light" ? "light" : "dark"}
                     dropdownPosition={{
                         side: "bottom",
                         align: "center",
