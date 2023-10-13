116ea64b9dcfe21fe5aab186ba1bd2d052f677f6
torof <scottpanam@protonmail.com>
2023-10-13 16:40:10 +0900

    simple deploy + testing

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -3,12 +3,12 @@ require("dotenv/config");
 
 require("dotenv/config");
 
-const RPC_URL_SEPOLIA = process.env.RPC_URL_SEPOLIA || "";
-const RPC_URL_MAINNET = process.env.RPC_URL_MAINNET;
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+// const RPC_URL_SEPOLIA = process.env.RPC_URL_SEPOLIA || "";
+// const RPC_URL_MAINNET = process.env.RPC_URL_MAINNET;
+// const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
+// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -19,39 +19,39 @@ module.exports = {
             runs: 200,
         },
     },
-    defaultNetwork: "sepolia",
+    defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            // chainId: 31337,
-            // gasPrice: 130000000000,
-            // forking: {
-            //     url: RPC_URL_MAINNET,
-            // },
-        },
+            chainId: 31337,
+            gasPrice: 130000000000,
+        //     forking: {
+        //         url: RPC_URL_MAINNET,
+        // },
+    },
         // goerli: {
         //     url: "https://rpc.ankr.com/eth_goerli",
         //     accounts: [PRIVATE_KEY],
         // },
-        sepolia: {
-            chainId: 11155111,
-            url: RPC_URL_SEPOLIA,
-            accounts: [PRIVATE_KEY],
-        },
+        // sepolia: {
+        //     chainId: 11155111,
+        //     url: RPC_URL_SEPOLIA,
+        //     accounts: [PRIVATE_KEY],
+        // },
     },
-    etherscan: {
-        apiKey: {
-            sepolia: ETHERSCAN_API_KEY,
-        },
-        customChains: [],
-    },
-    gasReporter: {
-        enabled: false,
-        outputFile: "report-gas.txt",
-        noColors: true,
-        currency: "USD",
-        coinmarketcap: COINMARKETCAP_API_KEY,
-        token: "ETH",
-    },
+    // etherscan: {
+    //     apiKey: {
+    //         sepolia: ETHERSCAN_API_KEY,
+    //     },
+    //     customChains: [],
+    // },
+    // gasReporter: {
+    //     enabled: false,
+    //     outputFile: "report-gas.txt",
+    //     noColors: true,
+    //     currency: "USD",
+    //     coinmarketcap: COINMARKETCAP_API_KEY,
+    //     token: "ETH",
+    // },
     paths: {
         artifacts: "./artifacts",
         cache: "./cache",
