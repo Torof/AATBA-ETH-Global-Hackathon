d90ab10be4556e4986d75bfab3fc880d3cc79e24
Kalpita Mandal <kalpita103@gmail.com>
2023-10-15 17:08:23 +0530

    Adding more tests

--- web3/contracts/SubProfile/EQUIP.sol
+++ web3/contracts/SubProfile/EQUIP.sol
@@ -6,35 +6,14 @@ import {SubProfileNFT} from "./SubProfileNFT.sol";
 import {WhitelistRegistry} from "./WhitelistRegistry.sol";
 
 contract EQUIP is IEQUIP, WhitelistRegistry {
+
     function verifyBadgeAndEquip(address senderContract, address from, uint256 tokenId, bytes memory data)
     public 
     returns(Badge memory) {
-        SubProfileNFT subProfile = SubProfileNFT(senderContract);
-        address token_address = address(subProfile);
-        VerificationStatus status = _verifyBadge(token_address);
-        return _Equip(token_address, from, tokenId, data, status);
+        bool verified = isWhitelisted(senderContract);
+        require(verified,"Sender Contract is not whitelisted");
+        Badge memory badge = Badge(senderContract, from, tokenId, data, verifyRequest.VERIFIED);
+        return badge;       
     }
 
-    function _verifyBadge(address sender) 
-    internal view
-    returns(VerificationStatus status) {
-        if (verificationRequests[sender].requestExists) {
-            if (isWhitelisted(sender)) {
-                status = VerificationStatus.VERIFIED;
-            } else {
-                status = VerificationStatus.PENDING;
-            }
-        } else {
-            status = VerificationStatus.NOT_VERIFIED;
-        }  
-    }
-
-    function _Equip(address nftAddress, address from, uint256 tokenId, bytes memory data, VerificationStatus status)
-    internal 
-    returns(Badge memory) {
-        require(status == VerificationStatus.VERIFIED, "Badge not verified");
-        Badge memory badge = Badge(nftAddress, from, tokenId, data, status);
-        emit BadgeReceived(tokenId, status);
-        return badge;
-    }
 }
--- web3/contracts/interfaces/IEQUIP.sol
+++ web3/contracts/interfaces/IEQUIP.sol
@@ -2,12 +2,17 @@
 pragma solidity ^0.8.0;
 
 interface IEQUIP {
-    event BadgeReceived(uint256 indexed tokenId, VerificationStatus status);
 
-    enum VerificationStatus {
-        PENDING,
+    enum verifyRequest {
+        REQUESTED,
         VERIFIED,
-        NOT_VERIFIED
+        NOT_VERIFIED, // verification fails
+        REMOVED
+    }
+
+    struct WhitelistRequest {
+        bool requestExists;
+        verifyRequest verifiedStatus;
     }
 
     struct Badge {
@@ -15,7 +20,7 @@ interface IEQUIP {
         address from;
         uint256 tokenId;
         bytes data; 
-        VerificationStatus status;
+        verifyRequest status;
     }
 
 }
