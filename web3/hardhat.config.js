c70d156eb6d9adc62de2b3d82d8c9223831a487c
torof <scottpanam@protonmail.com>
2023-10-17 16:09:56 +0900

    hhconfig

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -4,11 +4,11 @@ require("dotenv/config");
 
 require("dotenv/config");
 
-// const RPC_URL_SEPOLIA = process.env.RPC_URL_SEPOLIA || "";
+const RPC_URL_SEPOLIA = process.env.RPC_URL_SEPOLIA || "";
 // const RPC_URL_MAINNET = process.env.RPC_URL_MAINNET;
-// const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
 
-// const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 // const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -29,22 +29,18 @@ module.exports = {
         //         url: RPC_URL_MAINNET,
         // },
     },
-        // goerli: {
-        //     url: "https://rpc.ankr.com/eth_goerli",
-        //     accounts: [PRIVATE_KEY],
-        // },
-        // sepolia: {
-        //     chainId: 11155111,
-        //     url: RPC_URL_SEPOLIA,
-        //     accounts: [PRIVATE_KEY],
-        // },
+        sepolia: {
+            chainId: 11155111,
+            url: RPC_URL_SEPOLIA,
+            accounts: [PRIVATE_KEY],
+        },
     },
-    // etherscan: {
-    //     apiKey: {
-    //         sepolia: ETHERSCAN_API_KEY,
-    //     },
-    //     customChains: [],
-    // },
+    etherscan: {
+        apiKey: {
+            sepolia: ETHERSCAN_API_KEY,
+        },
+        customChains: [],
+    },
     // gasReporter: {
     //     enabled: false,
     //     outputFile: "report-gas.txt",
